c4318afb01a3179cd681c7f37f22e65d8e0cf733
Dinh Ky <dinhky0204@gmail.com>
2017-09-24 22:15:55 +0700

    create migrations and generate MVC user, genre, publisher, game

---
+++ db/migrate/20170924150344_create_games.rb
@@ -0,0 +1,15 @@
+class CreateGames < ActiveRecord::Migration[5.1]
+  def change
+    create_table :games do |t|
+      t.string :name
+      t.text :story
+      t.text :description
+      t.text :guide
+      t.string :photo
+      t.integer :publisher_id
+
+      t.timestamps
+    end
+    add_foreign_key :games, :publishers
+  end
+end
