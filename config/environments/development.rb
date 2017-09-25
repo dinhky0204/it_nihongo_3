8ada7e2c2db952273eb59de4bb2a3da130bf5ea8
Nguyen Dinh Manh <manhnd@kaopiz.com>
2017-09-25 17:49:36 +0700

    Add - add feature login, signup

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,9 +26,6 @@ Rails.application.configure do
     config.cache_store = :null_store
   end
 
-  # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
-
   config.action_mailer.perform_caching = false
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
@@ -53,4 +50,16 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  config.action_mailer.perform_deliveries = true
+  # Don't care if the mailer can't send.
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+      :address              => "smtp.gmail.com",
+      :port                 => 587,
+      :user_name            => 'sanni.itnihongo3@gmail.com',
+      :password             => 'sanni123456',
+      :authentication       => 'plain',
+      :enable_starttls_auto => true  }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,11 @@ Rails.application.routes.draw do
   resources :games
   resources :genres
   resources :publishers
-  devise_for :users
+  devise_for :users, controllers: {
+      registrations: 'users/registrations',
+  }
+  resources :users, only: :show
+
+  root :to => "games#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
