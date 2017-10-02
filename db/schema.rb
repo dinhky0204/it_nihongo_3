2303a1d2f2d17c6a980d20d0bfae8d32cc00ba8e
TranDieuLinh <linhttd.bk@gmail.com>
2017-10-03 03:16:58 +0700

    write review

--- db/schema.rb
+++ db/schema.rb
@@ -40,8 +40,8 @@ ActiveRecord::Schema.define(version: 20171002084023) do
     t.text "story"
     t.text "description"
     t.text "guide"
-    t.integer "publisher_id"
     t.string "photo"
+    t.integer "publisher_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
