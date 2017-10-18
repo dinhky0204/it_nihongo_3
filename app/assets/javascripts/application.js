eb36d2b5f40cf0b49835a759eb003a5ee12afe07
Dinh Ky <dinhky0204@gmail.com>
2017-10-18 14:58:35 +0700

    add gem react-rails

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,5 +1,5 @@
 // This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
+// listed below.//= require components
 //
 // Any JavaScript/Coffee file within this directory, lib/assets/javascripts, or any plugin's
 // vendor/assets/javascripts directory can be referenced here using a relative path.
@@ -23,3 +23,6 @@
 //= require materialize/extras/nouislider
 //= require bxslider
 //= require game
+//= require react
+//= require react_ujs
+//= require components
