82e65fdf6714d5c9df5a0b2da15ffea9fd685961
manhnd1112 <manhnd@kaopiz.com>
2017-10-28 17:44:43 +0700

    Mod - update feature notification

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,4 +2,22 @@ class Review < ApplicationRecord
   belongs_to :user
   belongs_to :game
   has_many :review_comments, -> { order "created_at DESC"}
+  after_update_commit {
+    if self.status == true
+      @followers = self.user.followers
+      puts "===============================================\n"
+      puts @followers;
+      puts "===============================================\n"
+      @followers.each do |follower|
+        @notification = @notification = Notification.new(
+            from_user_id: self.user.id,
+            to_user_id: follower.id,
+            type: Notification.types[:new_review],
+            review_id: self.id,
+            status: Notification.statuses[:not_seen]
+        )
+        @notification.save
+      end
+    end
+  }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -50,8 +50,8 @@ ActiveRecord::Schema.define(version: 20171018140652) do
     t.text "story"
     t.text "description"
     t.text "guide"
-    t.string "photo"
     t.integer "publisher_id"
+    t.string "photo"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
