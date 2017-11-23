c7f0ba219d4de1719eca127f08161de5a9e983a6
Dinh Ky <dinhky0204@gmail.com>
2017-11-23 11:33:32 +0700

    migrate update game table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171108062017) do
+ActiveRecord::Schema.define(version: 20171122070959) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,6 +56,7 @@ ActiveRecord::Schema.define(version: 20171108062017) do
     t.datetime "updated_at", null: false
     t.string "image"
     t.datetime "deleted_at"
+    t.integer "view", default: 0
   end
 
   create_table "genres", force: :cascade do |t|
